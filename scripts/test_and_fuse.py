f7b658a411c1f3105ff07a11c4a55bb01b00a67b
cpheinrich <cpheinrich@gmail.com>
2019-04-03 15:35:42 -0700

    more tweaks

--- scripts/test_and_fuse.py
+++ scripts/test_and_fuse.py
@@ -15,6 +15,7 @@ def main(args):
     all_urls = []
     start_time = time.time()
     ply_folder = os.path.join(args.ply_folder,str(start_time))
+    os.mkdir(ply_folder)
     for d in os.listdir(args.test_folder_root):
         dense_folder = os.path.join(args.test_folder_root, d)
         if args.no_test is not True:
@@ -32,10 +33,10 @@ if __name__ == '__main__':
     parser.add_argument('--test_folder_root', type=str, default='../data/7scenes/test/')
     parser.add_argument('--fusibile_path', type=str,
                         default='/home/chrisheinrich/fusibile/fusibile')
-    parser.add_argument('--prob_threshold', type=float, default='0.6')
+    parser.add_argument('--prob_threshold', type=float, default='0.8')
     parser.add_argument('--ply_folder', type=str,
                         default='/home/chrisheinrich/fused-point-clouds')
-    parser.add_argument('--disp_threshold', type=float, default='0.15')
+    parser.add_argument('--disp_threshold', type=float, default='0.25')
     parser.add_argument('--num_consistent', type=float, default='3')
     parser.add_argument('--no_test', action='store_true', help='Will not run testing, but only postprocessing, if flag is set')
     args = parser.parse_args()
