fea0b31ef0d7ec66501ae0b8bf7869648dfe794a
cpheinrich <cpheinrich@gmail.com>
2019-04-24 11:50:38 -0700

    thread tweaking

--- mvsnet/train.py
+++ mvsnet/train.py
@@ -284,7 +284,8 @@ def train(training_list=None, validation_list=None):
         init_op = tf.global_variables_initializer()
         config = tf.ConfigProto(allow_soft_placement=True)
         config.gpu_options.allow_growth = True
-        config.inter_op_parallelism_threads = 4;
+        config.inter_op_parallelism_threads = 0;
+        config.intra_op_parallelism_threads = 0
 
         with tf.Session(config=config) as sess:
 
