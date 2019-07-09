299a2d02ecb59f55c0a8d5614935b7148ad6bba8
cpheinrich <cpheinrich@gmail.com>
2019-07-08 20:10:24 -0700

    minor edit

--- mvsnet/inference.py
+++ mvsnet/inference.py
@@ -38,9 +38,9 @@ tf.app.flags.DEFINE_integer('ckpt_step', None,
 tf.app.flags.DEFINE_string('run_name', None,
                            """A name to use for wandb logging""")
 # input parameters
-tf.app.flags.DEFINE_integer('view_num', 6,
+tf.app.flags.DEFINE_integer('view_num', 4,
                             """Number of images (1 ref image and view_num - 1 view images).""")
-tf.app.flags.DEFINE_integer('max_d', 192,
+tf.app.flags.DEFINE_integer('max_d', 128,
                             """Maximum depth step when testing.""")
 tf.app.flags.DEFINE_integer('width', 512,
                             """Maximum image width when testing.""")
