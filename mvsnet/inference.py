598d9457bcb0d16a28ad2a2e0acd92c75286050c
cpheinrich <cpheinrich@gmail.com>
2019-07-22 14:34:18 -0700

    minor edit

--- mvsnet/inference.py
+++ mvsnet/inference.py
@@ -26,13 +26,13 @@ tf.app.flags.DEFINE_string('model_dir',
 tf.app.flags.DEFINE_integer('ckpt_step', None,
                             """ckpt  step.""")
 # input parameters
-tf.app.flags.DEFINE_integer('view_num', 8,
+tf.app.flags.DEFINE_integer('view_num', 6,
                             """Number of images (1 ref image and view_num - 1 view images).""")
-tf.app.flags.DEFINE_integer('max_d', 256,
+tf.app.flags.DEFINE_integer('max_d', 192,
                             """Maximum depth step when testing.""")
-tf.app.flags.DEFINE_integer('width', 1024,
+tf.app.flags.DEFINE_integer('width', 1600,
                             """Maximum image width when testing.""")
-tf.app.flags.DEFINE_integer('height', 768,
+tf.app.flags.DEFINE_integer('height', 1200,
                             """Maximum image height when testing.""")
 tf.app.flags.DEFINE_float('sample_scale', 0.25,
                           """Downsample scale for building cost volume (W and H).""")
