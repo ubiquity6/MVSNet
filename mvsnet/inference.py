dab566bf55ef716ce1210fd98f928775af4c5124
cpheinrich <cpheinrich@gmail.com>
2019-07-22 14:39:03 -0700

    minor edit

--- mvsnet/inference.py
+++ mvsnet/inference.py
@@ -28,7 +28,7 @@ tf.app.flags.DEFINE_integer('ckpt_step', None,
 # input parameters
 tf.app.flags.DEFINE_integer('view_num', 6,
                             """Number of images (1 ref image and view_num - 1 view images).""")
-tf.app.flags.DEFINE_integer('max_d', 192,
+tf.app.flags.DEFINE_integer('max_d', 128,
                             """Maximum depth step when testing.""")
 tf.app.flags.DEFINE_integer('width', 1536,
                             """Maximum image width when testing.""")
